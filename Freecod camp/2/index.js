19762ff922006d1baebf0fe157644276dcd7508a
mkkukul <mkkukul@gmail.com>
2024-01-02 21:10:36 +0300

    18

--- Freecod camp/2/index.js
+++ Freecod camp/2/index.js
@@ -1,7 +1,7 @@
 // document.getElementById("count").innerText = 5
 
 let count = 0
-console.log(count)
-
-let myAge = 35
+// console.log(count)
 console.log(myAge)
+let myAge = 35
+// console.log(myAge)
