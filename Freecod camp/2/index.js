236825404e243f1db0c9fd4af06c00cc9c623434
mkkukul <mkkukul@gmail.com>
2024-01-03 10:34:44 +0300

    3

--- Freecod camp/2/index.js
+++ Freecod camp/2/index.js
@@ -1,9 +1,13 @@
 // document.getElementById("count").innerText = 5
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch
 
 // console.log(count)
 // console.log(myAge)
 // let myAge = 35
 // console.log(myAge)
+
+let myAge = 35
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
