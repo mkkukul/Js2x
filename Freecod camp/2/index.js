1514a20eef163952757922c2610a90a43b91fafe
mkkukul <mkkukul@gmail.com>
2024-01-03 11:54:36 +0300

    345

--- Freecod camp/2/index.js
+++ Freecod camp/2/index.js
@@ -26,5 +26,5 @@ bonusPoints = bonusPoints+ 50
 console.log(bonusPoints)
 bonusPoints = bonusPoints- 75
 console.log(bonusPoints)
-bonusPoints = bonusPoints+45
-console.log(bonusPoints)
+// bonusPoints = bonusPoints+45
+// console.log(bonusPoints)
