fbccd215a3de37136402d963cc037d16c55814a4
mkkukul <mkkukul@gmail.com>
2024-01-03 22:34:11 +0300

    17

--- Freecod camp/12. Write a function that increments/index.js
+++ Freecod camp/12. Write a function that increments/index.js
@@ -2,7 +2,11 @@ let lapsCompleted = 0
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
-
+function laps (){
+    let lapsCompleted = lapsCompleted + 1
+    let lapsCompleted = lapsCompleted + 1
+    let lapsCompleted = lapsCompleted + 1
+}
 
 
 
