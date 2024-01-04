b0e0a243ea56f08d5fa42e70924c61d0070f4eba
mkkukul <mkkukul@gmail.com>
2024-01-04 23:39:55 +0300

    3.4.1

--- Freecod camp/Section 3/04. Your first if...else statement/index.js
+++ Freecod camp/Section 3/04. Your first if...else statement/index.js
@@ -6,3 +6,11 @@ let age = 22
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
 
+if (age < 21) {
+    console.log("Do you want to draw a new card?😐")
+} else if (age ===21){
+    console.log("Wohoo! You've got Blackjack🤑")
+
+}else if (sum> 21) {
+    console.log("You're out of the game!🥹")
+}
