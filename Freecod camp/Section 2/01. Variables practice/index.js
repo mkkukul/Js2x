059b4a66a3af34c5fd1f7106bc1430c7ef68d099
mkkukul <mkkukul@gmail.com>
2024-01-04 16:09:35 +0300

    JS  2 season full

--- Freecod camp/Section 2/01. Variables practice/index.js
+++ Freecod camp/Section 2/01. Variables practice/index.js
@@ -1,4 +1,8 @@
 // Create two variables, firstName and lastName
+let firstName = "Mustafa"
+let lastName = "Kemal"
+let fullName = firstName + " " + lastName
+console.log(fullName)
 
 // Concatenate the two variables into a third variable called fullName
 
--- Freecod camp/Section 2/06. Calculator challenge/index.js
+++ Freecod camp/Section 2/06. Calculator challenge/index.js
@@ -10,5 +10,27 @@ document.getElementById("num2-el").textContent = num2
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+let sumEl = document.getElementById("sum-el")
+function add(){
+    let result = num1 + num2;
+    sumEl.textContent = " Sum: " + result
+    // console.log("add clicked")
 
+}
+function subtract(){
+    let result = num1 - num2;
+    sumEl.textContent = " Sum: " + result
+    // console.log("subtract clicked")
+
+}
+function divide(){
+    let result = num1/num2;
+    sumEl.textContent = " Sum: " + result
+    // console.log("divide cliked")
+}
+function multiply(){
+    let result = num1*num2;
+    sumEl.textContent = " Sum: " + result
+    // console.log("multiply clicked")
+}
 
