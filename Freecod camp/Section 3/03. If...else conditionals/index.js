5f3bb93bd007b6846b3e9281866e0bbcae369f9e
mkkukul <mkkukul@gmail.com>
2024-01-04 23:36:02 +0300

    3.3.7

--- Freecod camp/Section 3/03. If...else conditionals/index.js
+++ Freecod camp/Section 3/03. If...else conditionals/index.js
@@ -3,7 +3,7 @@ let secondCard = 10
 let sum = firstCard + secondCard + 2
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+    console.log("Do you want to draw a new card?😐")
 } else if (sum ===21){
     console.log("Wohoo! You've got Blackjack")
 
