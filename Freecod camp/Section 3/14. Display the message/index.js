34ee974163fec2773b41ad59c270753dc098662c
mkkukul <mkkukul@gmail.com>
2024-01-05 21:40:02 +0300

    3.14.3

--- Freecod camp/Section 3/14. Display the message/index.js
+++ Freecod camp/Section 3/14. Display the message/index.js
@@ -12,7 +12,7 @@ function startGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack! 🥳"
         hasBlackJack = true
     } else {
         message = "You're out of the game! 😭"
