3fe4fd107f114abcecef29bfeeb629f3588a699a
mkkukul <mkkukul@gmail.com>
2024-01-06 21:13:13 +0300

    3.21.1

--- Freecod camp/Section 3/21. Intro to arrays/index.js
+++ Freecod camp/Section 3/21. Intro to arrays/index.js
@@ -8,4 +8,10 @@ let featuredPosts = [
 
 // Create an array that lists your i.e. experience, education, licenses, skills or similar
 // The items of the array should be strings
+let sections = [
+    "Experience",
+    "Education",
+    "Sciene"
+
+]
 
