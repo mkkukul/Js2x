8adad8333db7ae33c4b88cae3f7103926eb990e4
mkkukul <mkkukul@gmail.com>
2024-01-06 22:42:40 +0300

    3.23.0

--- Freecod camp/Section 3/23. Arrays with multiple data types/index.js
+++ Freecod camp/Section 3/23. Arrays with multiple data types/index.js
@@ -2,3 +2,5 @@
 
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
+const me = ["Alex", 25, true];
+console.log(me[2]);
