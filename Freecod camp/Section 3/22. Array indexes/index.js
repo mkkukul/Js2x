0a5567a245cfdf6f362dd26712f6b6fc656a2894
mkkukul <mkkukul@gmail.com>
2024-01-06 22:35:25 +0300

    3.21.4

--- Freecod camp/Section 3/22. Array indexes/index.js
+++ Freecod camp/Section 3/22. Array indexes/index.js
@@ -5,16 +5,16 @@ let featuredPosts = [
     "Here's the code for my project",//1
     "I've just relaunched my portfolio"//2
 ]
-
+console.log(featuredPosts.length )
 // console.log(featuredPosts[2])
-let experience = [
-    "CEO at Scrimba", 
-    "Frontend developer at Xeneta",
-    "People counter for Norstat"
-]
-console.log(experience[1])
-console.log(experience[2])
-console.log(experience[0])
+// let experience = [
+    // "CEO at Scrimba", 
+    // "Frontend developer at Xeneta",
+    // "People counter for Norstat"
+// ]
+// console.log(experience[1])
+// console.log(experience[2])
+// console.log(experience[0])
 
 
 // Make the following appear in the console:
