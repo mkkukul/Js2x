86ac95fed05f3ab45995dd942ec9a14537829bb5
mkkukul <mkkukul@gmail.com>
2024-01-06 23:13:11 +0300

    3.24.6

--- Freecod camp/Section 3/24. Adding and removing items from arrays/index.js
+++ Freecod camp/Section 3/24. Adding and removing items from arrays/index.js
@@ -20,7 +20,7 @@ cards.push(6)
 // modifyMessage()
 // 
 let messages = [
-    "Hey, how's it going?",
+    "Hey, how's it going?" ,
     "I'm great, thank you! How about you?",
     "All good. Been working on my portfolio lately."
 ];
