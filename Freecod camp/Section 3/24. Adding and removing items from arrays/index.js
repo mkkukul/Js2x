4e5f9409b8c65b0f591690ecf608fe427ca685bb
mkkukul <mkkukul@gmail.com>
2024-01-06 23:10:00 +0300

    3.24.3

--- Freecod camp/Section 3/24. Adding and removing items from arrays/index.js
+++ Freecod camp/Section 3/24. Adding and removing items from arrays/index.js
@@ -5,16 +5,17 @@ cards.push(6)
 
 // Push the newMessage to the messages array, and then log out the array
 
-let messages = [
-    "Hey, how's it going?",        
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately."
-]
-
-let newMessage = "Same here!"
-function(newMessage){
-    newMessage = newMessage + messages[1]
-    console.log(newMessage)
-
-}
-modifyMessage()
+// let messages = [
+    // "Hey, how's it going?",        
+    // "I'm great, thank you! How about you?",
+    // "All good. Been working on my portfolio lately."
+// ]
+// 
+// let newMessage = "Same here!"
+// function(newMessage){
+    // newMessage = newMessage + messages[1]
+    // console.log(newMessage)
+// 
+// }
+// modifyMessage()
+// 
