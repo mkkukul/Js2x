0bc170e1950dd2c303c29501ad3eb18dd1766858
mkkukul <mkkukul@gmail.com>
2024-01-07 21:11:18 +0300

    3.24.10

--- Freecod camp/Section 3/24. Adding and removing items from arrays/index.js
+++ Freecod camp/Section 3/24. Adding and removing items from arrays/index.js
@@ -26,8 +26,7 @@ let messages = [
 ];
 
 let newMessage = "Same here!";
-messages.push(newMessage); 
-// Yeni mesajı diziye ekliyoruz.
+messages.push(newMessage); // Yeni mesajı diziye ekliyoruz.
 
 console.log(messages);
 
