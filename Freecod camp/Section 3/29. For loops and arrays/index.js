555e9f664fb74c54445d83b3b16eaa3a98707037
mkkukul <mkkukul@gmail.com>
2024-01-07 22:12:03 +0300

    3.29.0

--- Freecod camp/Section 3/29. For loops and arrays/index.js
+++ Freecod camp/Section 3/29. For loops and arrays/index.js
@@ -5,8 +5,8 @@ let messages = [
     "Same here!",
     "Great to hear"
 ]
-
-// DRY - Don't repeat yourself
+console.log(messages)
+// DRY - Don't repeat yourself //! kendini tekramalayan kodlar yazın
 // console.log(messages[0])
 // console.log(messages[1])
 // console.log(messages[2])
