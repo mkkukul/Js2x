796ca55002655e9909ea083525d84f3df999274a
mkkukul <mkkukul@gmail.com>
2024-01-07 23:12:35 +0300

    3.30.4

--- Freecod camp/Section 3/31. For loops, arrays, and DOM/index.js
+++ Freecod camp/Section 3/31. For loops, arrays, and DOM/index.js
@@ -1,4 +1,4 @@
-let sentence = ["Hello", "my", "name ", "is ", "Per"] 
+let sentence = ["Hello", "my", "name", "is", "Per"] 
 let greetingEl = document.getElementById("greeting-el")
 
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
