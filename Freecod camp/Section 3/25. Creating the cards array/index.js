d310cda8d0fcd05115e2885e9ea2553afba40e3f
mkkukul <mkkukul@gmail.com>
2024-01-07 21:25:52 +0300

    3.25.2

--- Freecod camp/Section 3/25. Creating the cards array/index.js
+++ Freecod camp/Section 3/25. Creating the cards array/index.js
@@ -1,7 +1,7 @@
 let firstCard = 10
 let secondCard = 4
 // 1. Create a new array - cards - that contains firstCard and secondCard
-let cards = [firstCard, secondCard] // kartların arrayli gösterimini yaptık
+let cards = [firstCard, secondCard] // kartların arrayli hallerini yazarak gösterimini yaptık
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
