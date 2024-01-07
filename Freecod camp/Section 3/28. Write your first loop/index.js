2d8853c31bd0d73a60f2a2d3e3a92aa98fda3b54
mkkukul <mkkukul@gmail.com>
2024-01-07 22:05:15 +0300

    3.28.0

--- Freecod camp/Section 3/28. Write your first loop/index.js
+++ Freecod camp/Section 3/28. Write your first loop/index.js
@@ -1,4 +1,9 @@
 
-
+for (let count = 0; count < 101; count +=10 ) {
+    console.log(count)
+}
 // Create a for loop that counts from 10 to 100 in steps of 10
 // Use console.log to log out the numbers
+for (let i = 0; i < 6; i += 1 ) {
+    console.log(i)
+}
