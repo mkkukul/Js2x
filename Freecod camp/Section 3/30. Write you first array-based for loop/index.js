369c1be6c7022b229c01f1eae64c67558f69dbb2
mkkukul <mkkukul@gmail.com>
2024-01-07 23:06:09 +0300

    3.30.3

--- Freecod camp/Section 3/30. Write you first array-based for loop/index.js
+++ Freecod camp/Section 3/30. Write you first array-based for loop/index.js
@@ -2,7 +2,9 @@ let cards = [7, 3, 9]
 for(let i=0; i<3; i+=1 ){
     console.log(cards [i])
 }
-
+for(let i=0; i< cards.length; i++ ){
+    console.log(cards[i])
+}
 
 // Create a for loop that logs out all the cards in the array
 // Use cards.length to specify how long the loop should run
