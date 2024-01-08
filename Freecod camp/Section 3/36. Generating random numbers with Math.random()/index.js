6316bd3256b91793467082f8b7f8605dbe11894e
mkkukul <mkkukul@gmail.com>
2024-01-08 12:22:53 +0300

    3.36.0

--- Freecod camp/Section 3/36. Generating random numbers with Math.random()/index.js
+++ Freecod camp/Section 3/36. Generating random numbers with Math.random()/index.js
@@ -6,7 +6,8 @@ console.log(randomNumber)
 /* 
 
 What does Math.random() do?
+JavaScript'te 0 (dahil) ile 1 (hariç) arasında rastgele bir ondalıklı sayı üretir. Yani bu fonksiyon her seferinde farklı bir sayı döndürebilir ve bu değerler her zaman 0 ile 1 arasındadır.
 
-Your answer: 
+Your answer: 0/ 1 arasında bir değer verir
 
 */
