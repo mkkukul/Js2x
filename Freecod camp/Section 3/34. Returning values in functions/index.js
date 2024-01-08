dfa065a12accc2d5bc4ef132ffe0f5d35d954d54
mkkukul <mkkukul@gmail.com>
2024-01-08 11:30:27 +0300

    3.34.1

--- Freecod camp/Section 3/34. Returning values in functions/index.js
+++ Freecod camp/Section 3/34. Returning values in functions/index.js
@@ -15,9 +15,27 @@ function getFastestRaceTime() {
 let fastestRace = getFastestRaceTime()
 console.log(fastestRace)
 
+function getFastestRaceTime1() {
+    if (player1Time < player2Time) {
+        return player1Time;  // Değer döndürülür.
+    } else if (player2Time < player1Time) {
+        return player2Time;  // Değer döndürülür.
+    } else {
+        return player1Time;  // Değer döndürülür.
+    }
+}
+
+let fastestRace1 = getFastestRaceTime1();  // Fonksiyon çağrılır ve değeri döndürülür.
+console.log(fastestRace1);  // Döndürülen değer yazdırılır.
+
+
 // Write a function that returns the total race time
 // Call/invoke the function and store the returned value in a new variable
 // Finally, log the variable out
 
-
+function getTotalRaceTime(){
+    return player1Time + player2Time;
+}
+let totalTime = getTotalRaceTime()
+console.log(totalTime)
 
