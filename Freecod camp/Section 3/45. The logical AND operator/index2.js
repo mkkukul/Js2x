02bec4d62c32a89280464cdf93fce7f3a0ba77ee
mkkukul <mkkukul@gmail.com>
2024-01-09 10:56:15 +0300

    3.45.2.1

--- Freecod camp/Section 3/45. The logical AND operator/index2.js
+++ Freecod camp/Section 3/45. The logical AND operator/index2.js
@@ -16,3 +16,20 @@ if (hasCompletedCourse === true) {
 function generateCertificate() {
     console.log("Generating certificate....");
 }
+
+// hasCompletedCourse adında bir değişken oluşturulur ve bu değişken true değerine sahiptir.
+let hasCompletedCourse = true;
+
+// givesCertificate adında bir başka değişken oluşturulur ve bu değişken de true değerine sahiptir.
+let givesCertificate = true;
+
+// Eğer hasCompletedCourse ve givesCertificate değişkenleri de true ise, içerideki kod bloğuna girilir.
+if (hasCompletedCourse === true && givesCertificate === true) {
+    // generateCertificate() fonksiyonu çağrılır.
+    generateCertificate();
+}
+
+// generateCertificate adında bir fonksiyon tanımlanır. Bu fonksiyon, "Generating certificate...." mesajını konsola yazdırır.
+function generateCertificate() {
+    console.log("Generating certificate....");
+}
