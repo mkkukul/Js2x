1fdd5f54102ff83d8b8ed40c979beaed9ed1a5e9
mkkukul <mkkukul@gmail.com>
2024-01-09 15:36:50 +0300

    3.51.0

--- Freecod camp/Section 3/51. Create your first object/index.js
+++ Freecod camp/Section 3/51. Create your first object/index.js
@@ -3,5 +3,10 @@
 // Log out at least two of the keys using the dot notation
 
 let castle = {
+    title: "Live like a Kinh in my castle  ", 
+    price: 190,
+    isSuperHost:true;
+    images:  
+     
 
 }
