d5226ca242f6656582d0bca2c8a363b275a07508
mkkukul <mkkukul@gmail.com>
2024-01-09 13:38:35 +0300

    3.47.2

--- Freecod camp/Section 3/47. The logical OR operator/index.js
+++ Freecod camp/Section 3/47. The logical OR operator/index.js
@@ -12,7 +12,10 @@
     // console.log("Generating certificate....")
 // }
 let likesDocumentaries = true
-let likesStartups = true
+let likesStartups = false
+if ( likesDocumentaries === true || likesStartups === true){
+    recommendMovie()
+}
 function recommendMovie() {
     console.log("Hey, check out this new film we think you will like!")
 }
