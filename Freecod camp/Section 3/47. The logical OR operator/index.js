1d8647346343cde09c0495856109c898ba5eca74
mkkukul <mkkukul@gmail.com>
2024-01-09 13:34:52 +0300

    3.47.0

--- Freecod camp/Section 3/47. The logical OR operator/index.js
+++ Freecod camp/Section 3/47. The logical OR operator/index.js
@@ -1,6 +1,18 @@
 // Create two boolean variables, likesDocumentaries and likesStartups
 // Use an OR statement (||) to call recommendMovie() if either of those variables are true
 
+// let hasCompletedCourse = true
+// let givesCertificate = true
+// 
+// if (hasCompletedCourse === true || givesCertificate === true) {
+    // generateCertificate()
+// }
+// 
+// function generateCertificate() {
+    // console.log("Generating certificate....")
+// }
+let likesDocumentaries = true
+let likesStartups = true
 function recommendMovie() {
     console.log("Hey, check out this new film we think you will like!")
 }
