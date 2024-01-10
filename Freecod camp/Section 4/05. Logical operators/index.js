e6151efe0e875988fdabca2f9f287cc1ccd1f7ea
mkkukul <mkkukul@gmail.com>
2024-01-10 11:43:51 +0300

    4.5.2

--- Freecod camp/Section 4/05. Logical operators/index.js
+++ Freecod camp/Section 4/05. Logical operators/index.js
@@ -1,9 +1,16 @@
-let dayOfMonth = 13
+let dayOfMonth = 14
 let weekday = "Friday"
 
 // If it is Friday the 13th, log out this spooky face: 😱
 // Use the logical "AND operator" -> &&
 
-dayOfMonth.pop()
-dayOfMonth.push(31)
+// dayOfMonth.pop()
+// dayOfMonth.push(31)
+if( dayOfMonth === 13 && weekday === "Friday"  ){
+    console.log("😱")
+}else if (dayOfMonth < 13 ){
+    console.log("😊")
+}else if ( 13 < dayOfMonth) {
+    console.log("🥹")
+}
 
