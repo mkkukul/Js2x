a795307a4da0e0c44cc7ea41feab73d965a2d5b0
mkkukul <mkkukul@gmail.com>
2024-01-11 11:11:00 +0300

    5.14.0-5.13.0

--- Freecod camp/Section 5/13. Create the unordered list/index.js
+++ Freecod camp/Section 5/13. Create the unordered list/index.js
@@ -1,16 +1,14 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-// 2. Grab the unordered list and store it in a const variable called ulEl
 const ulEl = document.getElementById("ul-el")
-console.log(ulEl)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
-
+// Render the leads in the unordered list using ulEl.textContent
 for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i])
 }
