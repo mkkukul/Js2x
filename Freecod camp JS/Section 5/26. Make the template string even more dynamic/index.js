4011e384176021d86b79480d1c2266920779a2e2
mkkukul <mkkukul@gmail.com>
2024-01-12 12:45:26 +0300

    5.26.0

--- Freecod camp JS/Section 5/26. Make the template string even more dynamic/index.js
+++ Freecod camp JS/Section 5/26. Make the template string even more dynamic/index.js
@@ -1,9 +1,10 @@
 // template strings/literals
 
 const recipient = "James"
+const name = "Tako"
 // Create a new variable, sender, and set its value to your name
 
 // Use your sender variable instead of "Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `Hey ${recipient}! How is it going? Cheers ${name}`
 
 console.log(email)
