f1aaaf40a26cc4444d9de707c08411a8f80eefde
mkkukul <mkkukul@gmail.com>
2024-01-12 12:17:11 +0300

    5.24.0.1.2

--- Freecod camp JS/Section 5/24. Template strings/index.js
+++ Freecod camp JS/Section 5/24. Template strings/index.js
@@ -1,18 +1,23 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        // Tek tırnak ve çift tırnak kullanımını dikkatli bir şekilde yönetmelisiniz.
+        // Burada template literals (``) kullanarak daha okunabilir bir şekilde kod yazıyorum.
+        listItems += `
+            <li>
+                <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
+            </li>`;
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems;
 }
