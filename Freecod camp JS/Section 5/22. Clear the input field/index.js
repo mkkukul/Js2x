525f2a8195160afaccbb15e3fb45a57a3da15449
mkkukul <mkkukul@gmail.com>
2024-01-12 10:33:27 +0300

    5.23.1

--- Freecod camp JS/Section 5/22. Clear the input field/index.js
+++ Freecod camp JS/Section 5/22. Clear the input field/index.js
@@ -1,19 +1,36 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+// Boş bir dizi oluşturuluyor. Bu dizi, kullanıcının girdiği URL'leri saklamak için kullanılacak.
+let myLeads = [];
 
+// HTML'deki "input-el" ID'li input elementine erişim sağlanıyor.
+const inputEl = document.getElementById("input-el");
+
+// HTML'deki "input-btn" ID'li butona erişim sağlanıyor.
+const inputBtn = document.getElementById("input-btn");
+
+// HTML'deki "ul-el" ID'li unordered list (sırasız liste) elementine erişim sağlanıyor.
+const ulEl = document.getElementById("ul-el");
+
+// Butona tıklama olayı ekleniyor.
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    // Clear out the input field
-    inputEl.value = ""
-    renderLeads()
-})
+    // Kullanıcının girdiği URL, myLeads dizisine ekleniyor.
+    myLeads.push(inputEl.value);
+    
+    // Giriş alanı temizleniyor: Kullanıcının girdiği URL, giriş alanından kaldırılıyor.
+    inputEl.value = "";
 
+    // URL'ler sıralı bir şekilde gösteriliyor.
+    renderLeads();
+});
+
+// URL'leri sıralı bir şekilde göstermek için bu fonksiyon kullanılıyor.
 function renderLeads() {
-    let listItems = ""
+    let listItems = "";
+    
+    // myLeads dizisindeki her bir URL için bir <li> öğesi oluşturuluyor.
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li>" + myLeads[i] + "</li>";
     }
-    ulEl.innerHTML = listItems  
+    
+    // Oluşturulan <li> öğeleri, "ul-el" ID'li sırasız listeye ekleniyor.
+    ulEl.innerHTML = listItems;
 }
