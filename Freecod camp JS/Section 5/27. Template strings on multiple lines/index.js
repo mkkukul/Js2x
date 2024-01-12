0a5192057839236f8227b5a32a68cc46696a794e
mkkukul <mkkukul@gmail.com>
2024-01-12 12:51:34 +0300

    5.27.0

--- Freecod camp JS/Section 5/27. Template strings on multiple lines/index.js
+++ Freecod camp JS/Section 5/27. Template strings on multiple lines/index.js
@@ -1,9 +1,17 @@
 // template strings/literals
 
 const recipient = "James"
-const sender = "Per Harald Borgen"
+const sender = `
+Per 
+Harald
+Borgen`
 
 // Break the email string into multiple lines
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+const email = `
+Hey ${recipient}! 
+How is it going? 
+Cheers 
+${sender}
+`
 
 console.log(email)
