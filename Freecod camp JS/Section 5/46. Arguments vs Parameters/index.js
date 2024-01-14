005be038b1752a9302ef7d56ddbbe7cea957f43d
mkkukul <mkkukul@gmail.com>
2024-01-14 22:43:35 +0300

    5.46.0

--- Freecod camp JS/Section 5/46. Arguments vs Parameters/index.js
+++ Freecod camp JS/Section 5/46. Arguments vs Parameters/index.js
@@ -1,21 +1,19 @@
-// What are greeting and name?
-// What are "Howdy" and "James"?
-// What are num1 and num2?
-// What are 3 and 4?
-
-
-//.                parameters
+// Fonksiyonlar tanımlanırken parametreler belirtilir.
+// greetUser fonksiyonu, greeting ve name adında iki parametre alır.
 function greetUser(greeting, name) { 
-    welcomeEl.textContent = `${greeting}, ${name} 👋`
+    // Fonksiyon içindeki işlem: welcomeEl'in içeriğini parametrelerle günceller.
+    welcomeEl.textContent = `${greeting}, ${name} 👋`;
 }
 
-//.        arguments
-let hi = "Howdy"
-greetUser(hi, "James")
+// greetUser fonksiyonu çağrılırken, hi değişkeni greeting'e, "James" string'i ise name'e atanır.
+let hi = "Howdy";
+greetUser(hi, "James");
 
-
+// add fonksiyonu, num1 ve num2 adında iki parametre alır.
+// Fonksiyon, parametrelerin toplamını döndürür.
 function add(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 }
 
-add(3, 4)
+// add fonksiyonu çağrılırken, 3 num1'e, 4 ise num2'ye atanır ve sonuç elde edilir.
+add(3, 4);
