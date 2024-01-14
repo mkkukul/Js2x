b8faa467e065d68dbeb02829c6ade4df7d402de2
mkkukul <mkkukul@gmail.com>
2024-01-14 22:03:47 +0300

    5.42.00

--- Freecod camp JS/Section 5/41. Make the delete button work/index.js
+++ Freecod camp JS/Section 5/41. Make the delete button work/index.js
@@ -11,8 +11,8 @@ if (leadsFromLocalStorage) {
     renderLeads()
 }
 
-// 2. Listen for double clicks on the delete button (google it!)
-// 3. When clicked, clear localStorage, myLeads, and the DOM
+
+
 deleteBtn.addEventListener("dblclick",function(){
     localStorage.clear()
     myLeads =[]
