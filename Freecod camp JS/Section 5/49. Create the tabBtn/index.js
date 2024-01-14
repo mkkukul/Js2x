c8c2f0ff7550db71e3e7791311977fe32d932855
mkkukul <mkkukul@gmail.com>
2024-01-14 23:10:25 +0300

    5.49.1

--- Freecod camp JS/Section 5/49. Create the tabBtn/index.js
+++ Freecod camp JS/Section 5/49. Create the tabBtn/index.js
@@ -5,16 +5,19 @@ const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 // 1. Grab the SAVE TAB button and store it in a tabBtn variable
-
+const tabBtn = document.getElementById("tab-btn")
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
 const tabs = [
-    {url:"https://www.linkedin.com/in/mkkukul/"}
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
 ]
 // 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
-
+tabBtn.addEventListener("cliclk",function(){
+    console.log(tabs[0])
+})
 
 function render(leads) {
     let listItems = ""
