397c2b91e0598c1d152bad9ec326436a06c40b3d
mkkukul <mkkukul@gmail.com>
2024-01-15 11:30:51 +0300

    6.3.0

--- Freecod camp JS/Section 6/03. save to localStorage/index.js
+++ Freecod camp JS/Section 6/03. save to localStorage/index.js
@@ -1,3 +1,9 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+// localStorage.setItem("name", "John Doe");
+// console.log(localStorage.getItem("name"));
+// localStorage.setItem("myCredits", "100")
+// console.log(myCredits)
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
