8c96471b345073ea6a5e9dd5fb4240c149cbb43c
mkkukul <mkkukul@gmail.com>
2024-01-20 13:03:09 +0300

    3

--- Javascript Eğitimleri/1 Değişkenler ve Veri Türleri/script.js
+++ Javascript Eğitimleri/1 Değişkenler ve Veri Türleri/script.js
@@ -1,2 +1,12 @@
 const  isim = "Malik"
-let lastName = " Hard" 
+let lastName = " Hard";
+lastName = "test2";
+console.log(lastName)
+
+
+const value = "Name" 
+const value2 = 34
+const value3 = "34"
+const value4 = true
+//! Array
+ 
