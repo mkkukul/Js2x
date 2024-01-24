756790ac75cd2c898a69b55505fbd617065786ba
mkkukul <mkkukul@gmail.com>
2024-01-24 10:31:22 +0300

    5

--- Javascript Eğitimleri/1 Değişkenler ve Veri Türleri/array.js
+++ Javascript Eğitimleri/1 Değişkenler ve Veri Türleri/array.js
@@ -12,3 +12,13 @@ let arr1 = [1,2]
 let arr2 = [3,4]
 let mergedArray = arr1.concat(arr2)
 console.log(mergedArray)
+let numbers2 = [2,4,6,8]
+let isEven = numbers2.every((num)=>{
+    return num %2 === 0;
+})
+console.log(isEven)
+let words = [ "hello", `world`, 'goodbye']
+let check = words.every((value)=>{
+    return value.length > 6
+})
+console.log(check)
