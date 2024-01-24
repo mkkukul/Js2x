1566d8ed3beee143a87c2c1d5f8a7446993b7838
mkkukul <mkkukul@gmail.com>
2024-01-24 10:25:03 +0300

    3

---
+++ Javascript Eğitimleri/1 Değişkenler ve Veri Türleri/array.js
@@ -0,0 +1,14 @@
+let colors = ["red", "green", "blue"]
+console.log(colors[2]) // 
+console.log(colors.at(1))
+let numbers = [10, 20, 30];
+console.log(numbers.at(-1)) // son değeri alır (30)
+// array.concat ()
+let fruit = ["apple", "banana"]
+let vegetable = ["carrot", "broccoli"];
+let combined = fruit.concat(vegetable)
+console.log(combined)
+let arr1 = [1,2]
+let arr2 = [3,4]
+let mergedArray = arr1.concat(arr2)
+console.log(mergedArray)
