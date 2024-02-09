f90dae3eaff05a6c0d7d7b15429fb9470ee73cac
mkkukul <mkkukul@gmail.com>
2024-02-09 21:31:40 +0300

    16

--- Freecod camp JS/Section 1/10. Write your first function/index.js
+++ Freecod camp JS/Section 1/10. Write your first function/index.js
@@ -1,6 +1,11 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
+function myLogger() {
+    console.log(42); // Konsola 42 değerini yazdırır
+}
+
+myLogger(); // Fonksiyon çağrıldığında, konsola 42 yazdırılır
 
 
 // function myLogger() {
