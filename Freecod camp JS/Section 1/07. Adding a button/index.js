fd09719d82793677317985bec87e2225866c01f5
mkkukul <mkkukul@gmail.com>
2024-02-09 21:26:26 +0300

    3

--- Freecod camp JS/Section 1/07. Adding a button/index.js
+++ Freecod camp JS/Section 1/07. Adding a button/index.js
@@ -8,6 +8,17 @@
 // Decrease it down to 25, and then finally increase it to 70
 // Console.log the value after each step
 
+let bonusPoints = 50; // bonusPoints adında bir değişken oluşturulur ve başlangıç değeri 50 olarak atanır
+console.log(bonusPoints); // bonusPoints değeri konsola yazdırılır (50)
+
+bonusPoints = bonusPoints + 50; // bonusPoints değerine 50 eklenir
+console.log(bonusPoints); // bonusPoints değeri konsola yazdırılır (100)
+
+bonusPoints = bonusPoints - 75; // bonusPoints değerinden 75 çıkarılır
+console.log(bonusPoints); // bonusPoints değeri konsola yazdırılır (25)
+
+bonusPoints = bonusPoints + 45; // bonusPoints değerine 45 eklenir
+console.log(bonusPoints); // bonusPoints değeri konsola yazdırılır (70)
 
 
 
