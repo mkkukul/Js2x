4f668209f41af66d18816f84579053e0fd554d66
mkkukul <mkkukul@gmail.com>
2024-02-09 21:33:31 +0300

    19

--- Freecod camp JS/Section 1/11. Write a function that logs the sum/index.js
+++ Freecod camp JS/Section 1/11. Write a function that logs the sum/index.js
@@ -1,4 +1,15 @@
 
+let lap1 = 34; // İlk tur zamanı
+let lap2 = 33; // İkinci tur zamanı
+let lap3 = 36; // Üçüncü tur zamanı
+
+// Tüm tur zamanlarının toplamını konsola yazdıran bir fonksiyon oluştur
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3; // Tüm tur zamanlarının toplamını hesaplar
+    console.log(totalTime); // Toplam tur zamanını konsola yazdırır
+}
+
+logLapTime(); // Fonksiyonu çağırır ve tüm tur zamanlarının toplamını konsola yazdırır
 
 
 // let lap1 = 34
