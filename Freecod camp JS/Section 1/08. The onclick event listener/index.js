770a49a6e5ab9b958759d637b4c66af360ce2a3a
mkkukul <mkkukul@gmail.com>
2024-02-09 21:27:22 +0300

    4

--- Freecod camp JS/Section 1/08. The onclick event listener/index.js
+++ Freecod camp JS/Section 1/08. The onclick event listener/index.js
@@ -8,10 +8,13 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+// 
 
-function increment(){
-    console.log("The button was clicked")
-}
 
+
+// function increment(){
+    // console.log("The button was clicked")
+// }
+// 
 
 
