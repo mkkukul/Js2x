987f1e7daca42bf299a96abcbaac7b6689548eb9
mkkukul <mkkukul@gmail.com>
2024-02-09 21:34:59 +0300

    22

--- Freecod camp JS/Section 1/12. Write a function that increments/index.js
+++ Freecod camp JS/Section 1/12. Write a function that increments/index.js
@@ -1,4 +1,17 @@
 
+let lapsCompleted = 0; // Yapılan tur sayısını tutan bir değişken oluştur
+
+// Yapılan tur sayısını bir artırarak değiştiren bir fonksiyon oluştur
+function incrementLapsCompleted() {
+    lapsCompleted = lapsCompleted + 1; // lapsCompleted değişkenini bir artır
+}
+
+// Fonksiyonu üç kez çağırarak tur sayısını artır
+incrementLapsCompleted(); // Birinci kez çağır
+incrementLapsCompleted(); // İkinci kez çağır
+incrementLapsCompleted(); // Üçüncü kez çağır
+
+console.log(lapsCompleted); // Güncellenmiş tur sayısını konsola yazdır
 
 
 // let lapsCompleted = 0
