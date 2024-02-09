a4105c4dc90db480249700e94f3272e5272ad85b
mkkukul <mkkukul@gmail.com>
2024-02-09 21:30:02 +0300

    11

--- Freecod camp JS/Section 1/09. Using functions to write less code/index.js
+++ Freecod camp JS/Section 1/09. Using functions to write less code/index.js
@@ -1,11 +1,14 @@
 // Setting up the the race 🏎 🏎 🏎
-function countdown (){
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
-}
+
+
+
+// function countdown (){
+    // console.log(5)
+    // console.log(4)
+    // console.log(3)
+    // console.log(2)
+    // console.log(1)
+// }
 
 // Setting up the the race
 countdown()
