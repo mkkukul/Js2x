fe3c665d46017eda3d7fbef60b18655f2c5cbf1e
mkkukul <mkkukul@gmail.com>
2024-02-10 22:40:31 +0300

    8

--- Freecod camp JS/Section 1/15. The Document Object Model/index.js
+++ Freecod camp JS/Section 1/15. The Document Object Model/index.js
@@ -1,4 +1,12 @@
 
+let countEl = document.getElementById("count-el"); // "count-el" ID'sine sahip HTML elementini seçer ve countEl değişkenine atar
+
+let count = 0; // Bir sayacı değişkeni tanımlar ve sıfıra ayarlar
+
+function increment() {
+    count = count + 1; // Sayacı bir artırır
+    countEl.innerText = count; // Güncellenmiş sayacı, seçilen HTML elementinin metin içeriği olarak ayarlar
+}
 
 
 // let countEl = document.getElementById("count-el")
