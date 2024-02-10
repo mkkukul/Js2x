00b0477655972b6b765f685224156e0128e86917
mkkukul <mkkukul@gmail.com>
2024-02-10 22:46:21 +0300

    19

--- Freecod camp JS/Section 1/17. Create the save button/index.js
+++ Freecod camp JS/Section 1/17. Create the save button/index.js
@@ -1,13 +1,13 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+// let countEl = document.getElementById("count-el")
+// let count = 0
 
-function increment() {
-    count = count + 1
-    countEl.innerText = count
-}
+// function increment() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
 
-// 1. Create a function, save(), which logs out the count when it's called
- function save(){
-    console.log(count)
- }
+//  1. Create a function, save(), which logs out the count when it's called
+//  function save(){
+//     console.log(count)
+//  }
 
