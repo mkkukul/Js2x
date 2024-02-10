09b6e4e90bf99ac0a6133281aa541260fc97c14a
mkkukul <mkkukul@gmail.com>
2024-02-10 22:42:49 +0300

    11

--- Freecod camp JS/Section 1/16. Display the count with innerText/index.js
+++ Freecod camp JS/Section 1/16. Display the count with innerText/index.js
@@ -1,10 +1,11 @@
-// grab the count-el element, store it in a countEl variable
-let count = 0
 
-function increment() {
-    count = count + 1
-    console.log(count)
-    // set countEl's innerText to the count
-}
+//  grab the count-el element, store it in a countEl variable
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     console.log(count)
+//  set countEl's innerText to the count
+// }
 
 
