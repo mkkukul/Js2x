a2008da18c99fc7cf1d78ee834c170a7880feafe
mkkukul <mkkukul@gmail.com>
2024-02-10 22:43:03 +0300

    13

--- Freecod camp JS/Section 1/16. Display the count with innerText/index.js
+++ Freecod camp JS/Section 1/16. Display the count with innerText/index.js
@@ -1,4 +1,13 @@
 
+let countEl = document.getElementById("count-el"); // "count-el" ID'sine sahip HTML elementini seçer ve countEl değişkenine atar
+
+let count = 0; // Bir sayacı değişkeni tanımlar ve sıfıra ayarlar
+
+function increment() {
+    count = count + 1; // Sayacı bir artırır
+    countEl.innerText = count; // Güncellenmiş sayacı, seçilen HTML elementinin metin içeriği olarak ayarlar
+}
+
 
 
 //  grab the count-el element, store it in a countEl variable
