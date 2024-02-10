b3a899845e84a673e875ec245911972b154c9918
mkkukul <mkkukul@gmail.com>
2024-02-10 22:46:56 +0300

    20

--- Freecod camp JS/Section 1/17. Create the save button/index.js
+++ Freecod camp JS/Section 1/17. Create the save button/index.js
@@ -1,3 +1,22 @@
+// count-el elementini seçin ve countEl değişkenine atayın
+let countEl = document.getElementById("count-el");
+
+// Bir sayacı değişkeni tanımlayın ve başlangıç değerini sıfıra ayarlayın
+let count = 0;
+
+// increment işlevini tanımlayın
+function increment() {
+    // Sayacı bir artırın
+    count = count + 1;
+    // Artırılmış sayacı countEl'in metin içeriği olarak ayarlayın
+    countEl.innerText = count;
+}
+
+// save işlevini tanımlayın
+function save() {
+    // Sayacın mevcut değerini konsola kaydedin
+    console.log(count);
+}
 
 
 
