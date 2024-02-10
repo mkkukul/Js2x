f25196aea94a9cbdb596e4ec7dafbbcd4426b4ac
mkkukul <mkkukul@gmail.com>
2024-02-10 22:38:38 +0300

    7

--- Freecod camp JS/Section 1/14. Display the count/index.js
+++ Freecod camp JS/Section 1/14. Display the count/index.js
@@ -4,6 +4,16 @@
 
 
 
+let countEl = document.getElementById("count-el"); // "count-el" ID'sine sahip HTML elementini seç ve countEl değişkenine atar
+
+console.log(countEl); // Seçilen HTML elementini konsola yazdırır
+
+let count = 0; // Bir sayacı değişkeni oluştur ve sıfıra ayarla
+
+function increment() {
+    count = count + 1; // Değişkeni bir artır
+    countEl.innerText = count; // Güncellenmiş değeri seçilen HTML elementinin metin içeriği olarak ayarlar
+}
 
 
 // let countEl = document.getElementById("count-el")
