c622553abef3917d7110e9b8db80c3f41d48d583
mkkukul <mkkukul@gmail.com>
2024-02-11 22:26:26 +0300

    4

--- Freecod camp JS/Section 1/19. Write your first string variable/index.js
+++ Freecod camp JS/Section 1/19. Write your first string variable/index.js
@@ -1,14 +1,14 @@
-let username = "  per"
+// let username = "  per"
 
-// console.log(username)
-let message = "You have tree new notifications"
+//  console.log(username)
+// let message = "You have tree new notifications"
 
-// console.log(message + " , " + username)
-let messageToUser = message + "," + username + "!"
+//  console.log(message + " , " + username)
+// let messageToUser = message + "," + username + "!"
 
-console.log(messageToUser)
-// function messageToUser(){
-    // messageToUser = message + ","+ username
-    // let messageToUser = "message + , + username"
-    // console.log(messageToUser)
-// }
+// console.log(messageToUser)
+//  function messageToUser(){
+//  messageToUser = message + ","+ username
+//  let messageToUser = "message + , + username"
+//  console.log(messageToUser)
+//  }
