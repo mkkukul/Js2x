c187702911090ea67ec5a242ebaa7f5a649f362a
mkkukul <mkkukul@gmail.com>
2024-02-11 23:25:58 +0300

    16

--- Freecod camp JS/Section 1/22. Render a welcome message/index.js
+++ Freecod camp JS/Section 1/22. Render a welcome message/index.js
@@ -1,11 +1,11 @@
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById("welcome-el")
+// //! Grab the welcome-el paragraph and store it in a variable called welcomeEl
+// let welcomeEl = document.getElementById("welcome-el")
 
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
+// //! Create two variables (name & greeting) that contains your name
+// //! and the greeting we want to render on the page
 
-// Render the welcome message using welcomeEl.innerText
-let name = "Per Harald Borgen"
-let greeting = "Welcome Back "
+// //! Render the welcome message using welcomeEl.innerText
+// let name = "Per Harald Borgen"
+// let greeting = "Welcome Back "
 
-welcomeEl.innerText = greeting + name
+// welcomeEl.innerText = greeting + name
