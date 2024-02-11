96d3fca6611be49f9231fc2d7a382dc8b46f4323
mkkukul <mkkukul@gmail.com>
2024-02-11 22:24:32 +0300

    2

--- Freecod camp JS/Section 1/18. What is a string_/index.js
+++ Freecod camp JS/Section 1/18. What is a string_/index.js
@@ -1,3 +1,19 @@
+// count-el elementini seç ve countEl değişkenine sakla
+let countEl = document.getElementById("count-el");
+
+let count = 0; // Bir sayacı değişkeni tanımla ve sıfıra ayarla
+
+function increment() {
+    count = count + 1; // Sayacı bir artır
+    countEl.innerText = count; // Sayacın güncel değerini count-el elementinin metin içeriği olarak ayarla
+}
+
+function save() {
+    console.log(count); // Sayacın güncel değerini konsola yazdır
+}
+
+
+
 // let countEl = document.getElementById("count-el")
 // let count = 0
 
