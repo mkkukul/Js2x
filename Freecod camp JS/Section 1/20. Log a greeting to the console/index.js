b3d755dbd78b545471eee5c8716eff5a87d8948b
mkkukul <mkkukul@gmail.com>
2024-02-11 22:53:14 +0300

    11

--- Freecod camp JS/Section 1/20. Log a greeting to the console/index.js
+++ Freecod camp JS/Section 1/20. Log a greeting to the console/index.js
@@ -4,6 +4,24 @@
 // Create a third variable, myGreeting, that contatenates the two strings
 // Log myGreeting to the console
 
+let selamlama = "Hi"; // Kullanıcıya yapılan selamı içeren bir değişken tanımla
+let ad = "My name is "; // Kullanıcının adını belirten bir değişken tanımla
+let me = "Ali Can Yılmaz"; // Kullanıcının adını içeren bir değişken tanımla
+
+// Kullanıcıya özel selamlama mesajını oluştur
+let myHakGreeting = selamlama + ", " + ad + me;
+
+// Oluşturulan selamlama mesajını konsola yazdır
+console.log(myHakGreeting); 
+
+let name = "Per Farnk "; // Kullanıcının adını içeren bir değişken tanımla
+let greeting = "Hi, my name is "; // Kendi adını belirten bir selamlama ifadesi içeren bir değişken tanımla
+
+// Kendi adını ve selamı içeren bir mesaj oluştur
+let myGreeting = greeting + ", " + name;
+
+// Oluşturulan mesajı konsola yazdır
+console.log(myGreeting);
 
 
 
