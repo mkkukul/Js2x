8b39db9e31bf6e55cb3ab2ebf46f66988ce5876e
mkkukul <mkkukul@gmail.com>
2024-02-11 23:42:17 +0300

    21

--- Freecod camp JS/Section 1/23. Improve the message with string concatenation/index.js
+++ Freecod camp JS/Section 1/23. Improve the message with string concatenation/index.js
@@ -11,3 +11,16 @@
 // welcomeEl.innerText = greeting + name
 // welcomeEl.innerText = welcomeEl.innerText + " 👋"
 // welcomeEl.innerText += "👋"
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Per Harald Borgen "
+let greeting = "Welcome back "
+let emoji = "👋"
+// welcomeEl.innerText = greeting + name + emoji
+
+// Add an emoji to the end!
+// WRITE YOUR CODE BELOW HERE
+// HINT: count = count + 1
+welcomeEl.innerText = greeting + name
+// welcomeEl.innerText = welcomeEl.innerText + " 👋"
+welcomeEl.innerText += "👋"
