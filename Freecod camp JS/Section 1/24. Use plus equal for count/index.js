f2f270b6719865bf9c225167adfa95c1632c4e00
mkkukul <mkkukul@gmail.com>
2024-02-12 20:14:21 +0300

    4

--- Freecod camp JS/Section 1/24. Use plus equal for count/index.js
+++ Freecod camp JS/Section 1/24. Use plus equal for count/index.js
@@ -1,3 +1,21 @@
+// count-el ID'sine sahip HTML öğesini seçer ve countEl değişkenine atar
+let countEl = document.getElementById("count-el");
+
+// Sayacı temsil eden bir değişken tanımlar ve başlangıç değerini 0 olarak atar
+let count = 0;
+
+// Sayacı bir artırır ve güncellenmiş değeri countEl'in metin içeriğine atar
+function increment() {
+    // Artırma işlemini kısaltılmış biçimde yapar
+    count += 1;
+    // Güncellenmiş sayac değerini countEl'in metin içeriğine atar
+    countEl.innerText = count;
+}
+
+// Sayacın mevcut değerini konsola yazdıran bir fonksiyon
+function save() {
+    console.log(count);
+}
 
 
 
