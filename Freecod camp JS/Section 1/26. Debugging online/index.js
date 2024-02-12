6cd91a19f5e590d1eba2af12f007a537dd88023e
mkkukul <mkkukul@gmail.com>
2024-02-12 20:21:22 +0300

    1

--- Freecod camp JS/Section 1/26. Debugging online/index.js
+++ Freecod camp JS/Section 1/26. Debugging online/index.js
@@ -1,16 +1,17 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
 
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
+// function increment() {
+//     count += 1
+//     countEl.innerText = count
+// }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-}
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+// }
 
-// Google:
-// innerText alternative mdn
+// // Google:
+// // innerText alternative mdn
+// // 
