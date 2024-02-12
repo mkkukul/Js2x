05b18b5f4bc06d91f195ad89b6a38cbf89a09ed7
mkkukul <mkkukul@gmail.com>
2024-02-12 20:15:38 +0300

    2

--- Freecod camp JS/Section 1/25. Create the save feature/index.js
+++ Freecod camp JS/Section 1/25. Create the save feature/index.js
@@ -11,9 +11,9 @@
 // function save() {
 //     let countStr = count + "-"
 //     saveEl.textContent += countStr
-//     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-//     // 3. Render the variable in the saveEl using innerText
-//     // NB: Make sure to not delete the existing content of the paragraph
+//2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+//3. Render the variable in the saveEl using innerText
+//NB: Make sure to not delete the existing content of the paragraph
 //     console.log(count)
 // }
 
