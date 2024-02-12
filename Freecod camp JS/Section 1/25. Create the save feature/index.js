6c8fe4bd1f43c3140ac818e5c4747219fc1f848a
mkkukul <mkkukul@gmail.com>
2024-02-12 20:15:51 +0300

    4

--- Freecod camp JS/Section 1/25. Create the save feature/index.js
+++ Freecod camp JS/Section 1/25. Create the save feature/index.js
@@ -1,3 +1,6 @@
+
+
+
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
 // let saveEl = document.getElementById("save-el")
 // let countEl = document.getElementById("count-el")
