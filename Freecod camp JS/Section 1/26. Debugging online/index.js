2e5b00cca7ecbd64f546d3bb0862485edf377d2d
mkkukul <mkkukul@gmail.com>
2024-02-12 20:22:31 +0300

    8

--- Freecod camp JS/Section 1/26. Debugging online/index.js
+++ Freecod camp JS/Section 1/26. Debugging online/index.js
@@ -1,19 +1,19 @@
-// save-el ID'sine sahip HTML öğesini seçer ve saveEl değişkenine atar
+//! save-el ID'sine sahip HTML öğesini seçer ve saveEl değişkenine atar
 let saveEl = document.getElementById("save-el");
 
-// count-el ID'sine sahip HTML öğesini seçer ve countEl değişkenine atar
+//! count-el ID'sine sahip HTML öğesini seçer ve countEl değişkenine atar
 let countEl = document.getElementById("count-el");
 
 // Sayaç değişkeni oluşturur ve başlangıç değerini 0 olarak atar
 let count = 0;
 
-// countEl'i 1 artırır ve güncellenmiş değeri sayfa üzerinde görüntüler
+//! countEl'i 1 artırır ve güncellenmiş değeri sayfa üzerinde görüntüler
 function increment() {
     count += 1;
     countEl.innerText = count;
 }
 
-// Sayaç değerini saveEl'in içeriğine ekler
+//! Sayaç değerini saveEl'in içeriğine ekler
 function save() {
     // Sayaç değerini bir stringe dönüştürür ve sonuna "-" ekler
     let countStr = count + " - ";
