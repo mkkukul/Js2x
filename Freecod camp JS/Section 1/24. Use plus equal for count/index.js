efeb3e49db78565045edce057dbe795eade60c22
mkkukul <mkkukul@gmail.com>
2024-02-12 20:13:45 +0300

    1

--- Freecod camp JS/Section 1/24. Use plus equal for count/index.js
+++ Freecod camp JS/Section 1/24. Use plus equal for count/index.js
@@ -1,14 +1,14 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+// let countEl = document.getElementById("count-el")
+// let count = 0
 
-function increment() {
-    // Change this to use the plus equal technique you've learned
-    count += 1
-    countEl.innerText = count
-}
+// function increment() {
+//     // Change this to use the plus equal technique you've learned
+//     count += 1
+//     countEl.innerText = count
+// }
 
-function save() {
-    console.log(count)
-}
+// function save() {
+//     console.log(count)
+// }
 
 
