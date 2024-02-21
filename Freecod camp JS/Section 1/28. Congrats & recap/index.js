c10a0f44f1977067564b1299cbd3324734a8b816
mkkukul <mkkukul@gmail.com>
2024-02-21 23:27:50 +0300

    10

--- Freecod camp JS/Section 1/28. Congrats & recap/index.js
+++ Freecod camp JS/Section 1/28. Congrats & recap/index.js
@@ -1,3 +1,20 @@
+let count = 0; // Sayacı başlatır
+let saveEl = document.getElementById("save-el"); // save-el ID'li öğeyi alır
+let countEl = document.getElementById("count-el"); // count-el ID'li öğeyi alır
+
+function increment() {
+    count += 1; // Sayacı bir artırır
+    countEl.textContent = count; // countEl öğesinin metin içeriğini günceller
+}
+
+function save() {
+    let countStr = count + " - "; // Sayacın değerini ve bir tire işaretini içeren bir dize oluşturur
+    saveEl.textContent += countStr; // saveEl öğesinin metin içeriğine sayıyı ekler
+    countEl.textContent = 0; // countEl öğesinin metin içeriğini sıfırlar
+    count = 0; // Sayacı sıfırlar
+}
+
+console.log("Let's count people on the subway!"); // Konsola bir mesaj yazdırır
 
 
 
