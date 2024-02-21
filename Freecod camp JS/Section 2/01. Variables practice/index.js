0983dfe0c6ae4273fba4c9755f98a3b9d6642d0d
mkkukul <mkkukul@gmail.com>
2024-02-21 23:29:05 +0300

    12

--- Freecod camp JS/Section 2/01. Variables practice/index.js
+++ Freecod camp JS/Section 2/01. Variables practice/index.js
@@ -1,10 +1,11 @@
 // Create two variables, firstName and lastName
 
 
-let firstName = "Mustafa"
-let lastName = "Kemal"
-let fullName = firstName + " " + lastName
-console.log(fullName)
+
+// let firstName = "Mustafa"
+// let lastName = "Kemal"
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
 
 // Concatenate the two variables into a third variable called fullName
 
