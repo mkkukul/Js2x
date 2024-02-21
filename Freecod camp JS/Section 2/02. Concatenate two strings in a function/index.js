6f685247d2dead95960e15480b5ef3aa4873c9bd
mkkukul <mkkukul@gmail.com>
2024-02-21 23:32:17 +0300

    19

--- Freecod camp JS/Section 2/02. Concatenate two strings in a function/index.js
+++ Freecod camp JS/Section 2/02. Concatenate two strings in a function/index.js
@@ -11,6 +11,12 @@ function full() {
 // full fonksiyonunu çağır, böylece konsola "Hi there, Linda!" yazısı yazdırılır
 full();
 
+name ve greeting değişkenleri oluşturulur ve sırasıyla "Linda" ve "Hi there" değerlerini içerir.
+full adında bir fonksiyon tanımlanır. Bu fonksiyon, greeting ve name değişkenlerini kullanarak bir selamlama mesajı oluşturur ve konsola yazdırır.
+Fonksiyon çağrısı full() ile yapılır, böylece konsola "Hi there, Linda!" mesajı yazdırılır.
+
+
+
 
 
 
