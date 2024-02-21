e315eada61e7e188c118a69e1a80d51c0c161e11
mkkukul <mkkukul@gmail.com>
2024-02-21 23:29:42 +0300

    13

--- Freecod camp JS/Section 2/01. Variables practice/index.js
+++ Freecod camp JS/Section 2/01. Variables practice/index.js
@@ -1,6 +1,19 @@
 // Create two variables, firstName and lastName
 
+// İlk adı ve soyadı değişkenlerini oluştur
+let firstName = "Mustafa";
+let lastName = "Kemal";
 
+// İki adı birleştirerek tam adı oluştur
+let fullName = firstName + " " + lastName;
+
+// Tam adı konsola yazdır
+console.log(fullName);
+
+
+// firstName ve lastName değişkenleri oluşturulur ve sırasıyla "Mustafa" ve "Kemal" adlarını içerir.
+//  fullName değişkeni, firstName ve lastName değişkenlerini birleştirerek tam adı oluşturur. Bu işlem "+" operatörü ile gerçekleştirilir ve araya bir boşluk eklenir.
+//  Oluşturulan tam ad, console.log fonksiyonu aracılığıyla konsola yazdırılır.
 
 // let firstName = "Mustafa"
 // let lastName = "Kemal"
