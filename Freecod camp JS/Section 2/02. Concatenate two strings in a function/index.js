8492900b8068a8b381205a6cfcd49865793a3a58
mkkukul <mkkukul@gmail.com>
2024-02-21 23:31:25 +0300

    14

--- Freecod camp JS/Section 2/02. Concatenate two strings in a function/index.js
+++ Freecod camp JS/Section 2/02. Concatenate two strings in a function/index.js
@@ -1,9 +1,9 @@
-let name = "Linda"
-let greeting = "Hi there"
+// let name = "Linda"
+// let greeting = "Hi there"
 
-// Create a function that logs out "Hi there, Linda!" when called
-function full(){
+// // Create a function that logs out "Hi there, Linda!" when called
+// function full(){
 
-    console.log(greeting+ " "+ name+"!");
-}
-full(); // Call the function to see what it does!</s>
+//     console.log(greeting+ " "+ name+"!");
+// }
+// full(); // Call the function to see what it does!</s>
