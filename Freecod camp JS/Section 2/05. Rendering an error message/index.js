5a63ed54357f45033a2fe7094462dff8c7e7f0ae
mkkukul <mkkukul@gmail.com>
2024-02-22 22:35:26 +0300

    9

--- Freecod camp JS/Section 2/05. Rendering an error message/index.js
+++ Freecod camp JS/Section 2/05. Rendering an error message/index.js
@@ -10,7 +10,12 @@ function purchase() {
     errorParagraph.textContent = "Something went wrong, please try again";
 }
 
-
+// let errorParagraph = document.getElementById("error");: HTML belgesindeki "error" id'ye sahip paragraf elementini seçer ve bu elementi errorParagraph değişkenine atar.
+//  console.log(errorParagraph);: Seçilen paragraf elementini konsola yazdırır. Bu, seçimin doğru olup olmadığını kontrol etmek için kullanılabilir.
+//  function purchase() { ... }: purchase adında bir fonksiyon tanımlar. Bu fonksiyon, bir düğmeye tıklandığında çalıştırılacak olan işlevi içerir.
+//  console.log("button clicked");: Düğmeye tıklama olayının gerçekleştiğini belirtmek için konsola "button clicked" mesajını yazdırır.
+// // // // errorParagraph.textContent = "Something went wrong, please try again";: errorParagraph adlı paragraf elementinin metin içeriğini değiştirir. Böylece, bir hata durumunda kullanıcıya uygun bir mesaj görüntülenir.
+// 
 
 // let errorParagraph = document.getElementById("error")
 // console.log(errorParagraph)
