b9bd650f3bc44357e7a54b2dfc255ea1fd4f6f91
mkkukul <mkkukul@gmail.com>
2024-02-22 22:41:14 +0300

    18

--- Freecod camp JS/Section 3/03. If...else conditionals/index.js
+++ Freecod camp JS/Section 3/03. If...else conditionals/index.js
@@ -1,3 +1,14 @@
+let firstCard = 11; // İlk kartın değeri
+let secondCard = 10; // İkinci kartın değeri
+let sum = firstCard + secondCard + 2; // Kartların toplam değeri hesaplanır
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?😐"); // Toplam 21'den küçükse, oyuncuya yeni bir kart çekmek isteyip istemediği sorulur
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack🤑"); // Toplam 21'e eşitse, oyuncuya Blackjack yaptığı bildirilir
+} else if (sum > 21) {
+    console.log("You're out of the game!🥹"); // Toplam 21'den büyükse, oyuncunun oyun dışı kaldığına dair bir mesaj verilir
+}
 
 
 
