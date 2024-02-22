0bda2ab19f339715a43a35476037fbea52316d49
mkkukul <mkkukul@gmail.com>
2024-02-22 22:29:53 +0300

    2

--- Freecod camp JS/Section 2/03. Incrementing and decrementing/index.js
+++ Freecod camp JS/Section 2/03. Incrementing and decrementing/index.js
@@ -1,3 +1,27 @@
+let myPoints = 3; // myPoints adında bir değişken oluşturulur ve başlangıçta 3 değeri atanır
+
+// add3Points adında bir fonksiyon tanımlanır. Bu fonksiyon, myPoints değişkenine 3 puan ekler
+function add3Points() {
+    myPoints += 3;
+}
+
+// remove1Point adında bir fonksiyon tanımlanır. Bu fonksiyon, myPoints değişkeninden 1 puan çıkarır
+function remove1Point() {
+    myPoints -= 1;
+}
+
+// add3Points fonksiyonu üç kez çağrılır, böylece myPoints değişkenine toplam 9 puan eklenir
+add3Points();
+add3Points();
+add3Points();
+
+// remove1Point fonksiyonu iki kez çağrılır, böylece myPoints değişkeninden toplam 2 puan çıkarılır
+remove1Point();
+remove1Point();
+
+// myPoints değişkeninin son değeri konsola yazdırılır, bu da 10 olmalıdır
+console.log(myPoints); // 10
+
 
 
 
