fb85245b61c8d375b48e3e89e9c27e1a4aeba256
mkkukul <mkkukul@gmail.com>
2024-02-22 22:39:13 +0300

    15

--- Freecod camp JS/Section 2/06. Calculator challenge/index.js
+++ Freecod camp JS/Section 2/06. Calculator challenge/index.js
@@ -19,6 +19,11 @@ document.getElementById("num2-el").textContent = num2;
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 
+
+
+
+
+
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 // let sumEl = document.getElementById("sum-el")
