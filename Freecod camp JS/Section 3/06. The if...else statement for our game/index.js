586aa0e20991188367d9637b3da2832a5ce069ed
mkkukul <mkkukul@gmail.com>
2024-02-23 19:48:37 +0300

    4

--- Freecod camp JS/Section 3/06. The if...else statement for our game/index.js
+++ Freecod camp JS/Section 3/06. The if...else statement for our game/index.js
@@ -24,3 +24,10 @@ let sum = firstCard + secondCard
 // }else if(sum>21){
 // console.log("You're out of the game! 😭")
 // }
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂"); // Toplam 21'den küçükse, "Yeni bir kart çekmek ister misiniz?" mesajı görüntülenir
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳"); // Toplam 21'e eşitse, "Wohoo! Blackjack yaptınız!" mesajı görüntülenir
+} else if (sum > 21) {
+    console.log("You're out of the game! 😭"); // Toplam 21'den büyükse, "Oyundan çıktınız!" mesajı görüntülenir
+}
