42ad669c367da2aa1dfd775a968bc494384c1ba5
mkkukul <mkkukul@gmail.com>
2024-02-24 22:16:53 +0300

    5

--- Freecod camp JS/Section 4/04. push, pop, unshift, shift challenge/index.js
+++ Freecod camp JS/Section 4/04. push, pop, unshift, shift challenge/index.js
@@ -12,3 +12,12 @@ largeCountries.shift()
 largeCountries.unshift("China")
 console.log(largeCountries)
 
+// largeCountries.pop(): largeCountries dizisinin sonundaki elemanı çıkarır. Bu işlem, dizinin sonundaki ülkeyi kaldırır.
+// 
+//  largeCountries.push("Pakistan"): largeCountries dizisinin sonuna "Pakistan" adlı bir ülke ekler. Bu işlem, dizinin sonuna yeni bir ülke ekler.
+// 
+//  largeCountries.shift(): largeCountries dizisinin başındaki elemanı çıkarır. Bu işlem, dizinin başındaki ülkeyi kaldırır.
+// 
+//  largeCountries.unshift("China"): largeCountries dizisinin başına "China" adlı bir ülke ekler. Bu işlem, dizinin başına yeni bir ülke ekler.
+// 
+// console.log(largeCountries): Değiştirilmiş largeCountries dizisini konsola yazdırır. Bu işlem, dizinin güncellenmiş halini görüntüler.
