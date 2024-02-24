09e31ce22939ca04389f49e2e40dd738b8237154
mkkukul <mkkukul@gmail.com>
2024-02-24 22:24:52 +0300

    21

--- Freecod camp JS/Section 5/04. Make the input button work with onclick/index.js
+++ Freecod camp JS/Section 5/04. Make the input button work with onclick/index.js
@@ -1,4 +1,9 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+// saveLead() fonksiyonu tanımlanır
+function saveLead() {
+    // "Button clicked!" metni konsola yazdırılır
+    console.log("Button clicked!");
+}
 
 
 
