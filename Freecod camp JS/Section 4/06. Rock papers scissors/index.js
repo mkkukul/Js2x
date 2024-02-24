ab6297d18c3584484faa11b2f483c821e81de77e
mkkukul <mkkukul@gmail.com>
2024-02-24 22:19:51 +0300

    13

--- Freecod camp JS/Section 4/06. Rock papers scissors/index.js
+++ Freecod camp JS/Section 4/06. Rock papers scissors/index.js
@@ -1,3 +1,15 @@
+let hands = ["rock", "paper", "scissor"];
+
+// Fonksiyon, "hands" dizisinden rastgele bir el işareti döndürür
+function getRandomHand() {
+    // Rastgele bir indeks seçilir
+    let randomIndex = Math.floor(Math.random() * hands.length);
+    // Seçilen indeksteki el işareti döndürülür
+    return hands[randomIndex];
+}
+
+// getRandomHand fonksiyonu çağrılarak rastgele bir el işareti konsola yazdırılır
+console.log(getRandomHand());
 
 
 
