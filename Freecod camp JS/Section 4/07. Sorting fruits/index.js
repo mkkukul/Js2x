58ccca661116c7421112ca510545e544dcf2c615
mkkukul <mkkukul@gmail.com>
2024-02-24 22:23:05 +0300

    16

--- Freecod camp JS/Section 4/07. Sorting fruits/index.js
+++ Freecod camp JS/Section 4/07. Sorting fruits/index.js
@@ -15,14 +15,14 @@ let orangeShelf = document.getElementById("orange-shelf");
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
-
-function sortFruit(){
-    for(let i = 0; i<fruit.length; i++){
-        if (fruit[i] === "🍎"){
-            appleShelf.textContent += "🍎"
-        } else if (fruit[i] === "🍊"){
-            orangeShelf.textContent += "🍊"
-        }      
-    }
-}
-sortFruit();
+// 
+// function sortFruit(){
+    // for(let i = 0; i<fruit.length; i++){
+        // if (fruit[i] === "🍎"){
+            // appleShelf.textContent += "🍎"
+        // } else if (fruit[i] === "🍊"){
+            // orangeShelf.textContent += "🍊"
+        // }      
+    // }
+// }
+// sortFruit();
