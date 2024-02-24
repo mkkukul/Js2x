835e264e498d37a12fa0303a103452a117b83395
mkkukul <mkkukul@gmail.com>
2024-02-24 22:19:43 +0300

    12

--- Freecod camp JS/Section 4/06. Rock papers scissors/index.js
+++ Freecod camp JS/Section 4/06. Rock papers scissors/index.js
@@ -1,10 +1,13 @@
-let hands = ["rock", "paper", "scissor"]
+
+
+
+// let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
-function getRandomHand(){
-    let randomIndex = Math.floor(Math.random()* hands.length);
-    return hands[randomIndex];
-     
-}
-console.log(getRandomHand());
+// function getRandomHand(){
+    // let randomIndex = Math.floor(Math.random()* hands.length);
+    // return hands[randomIndex];
+    //  
+// }
+// console.log(getRandomHand());
