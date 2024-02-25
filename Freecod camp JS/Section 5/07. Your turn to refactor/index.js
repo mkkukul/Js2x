f19f105f3002253250799066f9a5b15167eafd08
mkkukul <mkkukul@gmail.com>
2024-02-25 21:24:49 +0300

    16

--- Freecod camp JS/Section 5/07. Your turn to refactor/index.js
+++ Freecod camp JS/Section 5/07. Your turn to refactor/index.js
@@ -7,7 +7,7 @@ inputBtn.addEventListener("click", function(){
     console.log("Button Clicked!");
 })
 
-
+//! Bu kod, "SAVE INPUT" düğmesine tıklama olayını dinler ve tıklama gerçekleştiğinde "Button Clicked!" mesajını konsola yazdırır. .addEventListener() kullanımı, kodu daha açık ve okunabilir hale getirir ve JavaScript'te olayları ele almanın modern bir yoludur.
 
 
 // Refactor the code so that it uses .addEventListener()
