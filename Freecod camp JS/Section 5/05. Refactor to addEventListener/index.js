19f95aa45f181d1754b2e7a9b0d3de305b0d64de
mkkukul <mkkukul@gmail.com>
2024-02-25 21:18:49 +0300

    3

--- Freecod camp JS/Section 5/05. Refactor to addEventListener/index.js
+++ Freecod camp JS/Section 5/05. Refactor to addEventListener/index.js
@@ -1,3 +1,17 @@
+// saveLead() fonksiyonu tanımlanır
+function saveLead() {
+    // "Button clicked from onclick attribute!" metni konsola yazdırılır
+    console.log("Button clicked from onclick attribute!");
+}
+
+// inputBtn değişkeni, DOM'da "input-btn" id'sine sahip bir öğeyi alır
+let inputBtn = document.getElementById("input-btn");
+
+// inputBtn öğesi üzerinde 'click' olayını dinleyen bir olay dinleyici eklenir
+inputBtn.addEventListener('click', function() {
+    // "Button cliked from addEventListener" metni konsola yazdırılır
+    console.log("Button clicked from addEventListener");
+});
 
 
 
