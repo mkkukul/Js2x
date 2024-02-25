30ca321f79f5097344ebaf305320fbda77cb3e9f
mkkukul <mkkukul@gmail.com>
2024-02-25 21:26:00 +0300

    18

--- Freecod camp JS/Section 5/08. Create the myLeads array and inputEl/index.js
+++ Freecod camp JS/Section 5/08. Create the myLeads array and inputEl/index.js
@@ -1,3 +1,19 @@
+// Boş bir dizi oluşturulur ve 'myLeads' adlı değişkende saklanır
+let myLeads = [];
+
+// Giriş alanı ve giriş düğmesi HTML öğeleri alınır
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+// Giriş düğmesine tıklama olayı dinleyicisi eklenir
+inputBtn.addEventListener("click", function() {
+    // Bu işlev, giriş alanına girilen değeri alır ve 'myLeads' dizisine ekler
+    // Şu anda sadece düğmeye tıklandığında konsola bir mesaj yazdırır
+    // console.log("Button clicked!");
+});
+
+
+
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
