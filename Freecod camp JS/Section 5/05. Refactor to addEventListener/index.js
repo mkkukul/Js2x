b3a06cc7973a8bf48aa22171f985e6093b20471c
mkkukul <mkkukul@gmail.com>
2024-02-25 21:19:39 +0300

    6

--- Freecod camp JS/Section 5/05. Refactor to addEventListener/index.js
+++ Freecod camp JS/Section 5/05. Refactor to addEventListener/index.js
@@ -13,9 +13,9 @@ inputBtn.addEventListener('click', function() {
     console.log("Button clicked from addEventListener");
 });
 
-// İlk olarak, bir HTML düğmesine tıklama işlemini işlemek için bir saveLead() adlı bir işlev tanımlanır. Daha sonra, bu işlev, bir butona onclick özniteliği ile bağlanabilir.
+//! İlk olarak, bir HTML düğmesine tıklama işlemini işlemek için bir saveLead() adlı bir işlev tanımlanır. Daha sonra, bu işlev, bir butona onclick özniteliği ile bağlanabilir.
 // 
-//  İkinci olarak, aynı HTML düğmesine tıklama işlemini ele almak için addEventListener() yöntemi kullanılır. Bu yöntem, belirtilen bir olay dinleyiciyi (burada bir anonim işlev) belirtilen bir olay türüne (burada 'click') ekler. Bu yöntem, daha dinamik bir etkileşim sağlar ve genellikle tercih edilen bir yöntemdir.
+//!  İkinci olarak, aynı HTML düğmesine tıklama işlemini ele almak için addEventListener() yöntemi kullanılır. Bu yöntem, belirtilen bir olay dinleyiciyi (burada bir anonim işlev) belirtilen bir olay türüne (burada 'click') ekler. Bu yöntem, daha dinamik bir etkileşim sağlar ve genellikle tercih edilen bir yöntemdir.
 
 
 // function saveLead() {
