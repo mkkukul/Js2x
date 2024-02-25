67f63f42547200d5782b660a1c49853db3331707
mkkukul <mkkukul@gmail.com>
2024-02-25 21:23:39 +0300

    12

--- Freecod camp JS/Section 5/07. Your turn to refactor/index.js
+++ Freecod camp JS/Section 5/07. Your turn to refactor/index.js
@@ -1,7 +1,7 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 //! let inputBtn = document.getElementById("input-btn");
-// inputBtn.addEventListener("click", function(){
+//! inputBtn.addEventListener("click", function(){
     // console.log("Button Clicked!");
 // })
 // 
