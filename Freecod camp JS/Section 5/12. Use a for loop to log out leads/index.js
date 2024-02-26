52095b3399ebf6682eb2a002fd505cb42759f197
mkkukul <mkkukul@gmail.com>
2024-02-26 21:27:06 +0300

    17

--- Freecod camp JS/Section 5/12. Use a for loop to log out leads/index.js
+++ Freecod camp JS/Section 5/12. Use a for loop to log out leads/index.js
@@ -1,3 +1,17 @@
+let myLeads = [ "www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]; // Başlangıçta üç öğe içeren bir dizi
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+// inputBtn düğmesine tıklandığında myLeads dizisine inputEl'in değerini ekle
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+});
+
+// myLeads dizisindeki öğeleri bir döngü kullanarak konsola yazdır
+for(let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i]);
+}
 
 
 
