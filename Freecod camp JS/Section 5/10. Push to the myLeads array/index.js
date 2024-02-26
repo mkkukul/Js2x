e88a1144b912a1549133776174690ac80c7a0afa
mkkukul <mkkukul@gmail.com>
2024-02-26 21:24:04 +0300

    7

--- Freecod camp JS/Section 5/10. Push to the myLeads array/index.js
+++ Freecod camp JS/Section 5/10. Push to the myLeads array/index.js
@@ -1,11 +1,11 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
+// let myLeads = []
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
 
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+// // Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
-inputBtn.addEventListener("click", function() {
+// inputBtn.addEventListener("click", function() {
     
-    myLeads.push("www.awesomelead.com");
-    console.log(myLeads)
-})
+//     myLeads.push("www.awesomelead.com");
+//     console.log(myLeads)
+// })
