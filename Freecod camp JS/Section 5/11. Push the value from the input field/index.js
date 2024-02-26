8b61a0f0fea83fc8ba1fe33ee800a83e40e33451
mkkukul <mkkukul@gmail.com>
2024-02-26 21:25:20 +0300

    11

--- Freecod camp JS/Section 5/11. Push the value from the input field/index.js
+++ Freecod camp JS/Section 5/11. Push the value from the input field/index.js
@@ -3,9 +3,9 @@
 // const inputBtn = document.getElementById("input-btn")
 
 // inputBtn.addEventListener("click", function() {
-     // Push the value from the inputEl into the myLeads array 
-     // instead of the hard-coded "www.awesomeleads.com" value
-     // Google -> "get value from input field javascript"
+     //! Push the value from the inputEl into the myLeads array 
+     //! instead of the hard-coded "www.awesomeleads.com" value
+     //! Google -> "get value from input field javascript"
 //     myLeads.push(inputEl.value)
 //     console.log(myLeads)
 // })
