b44e128bc923e5a183ecf7ff3dd03f032e5f977b
mkkukul <mkkukul@gmail.com>
2024-02-26 21:14:59 +0300

    6

--- Freecod camp JS/Section 5/09. When to use let and const/index.js
+++ Freecod camp JS/Section 5/09. When to use let and const/index.js
@@ -1,20 +1,20 @@
-// If possible, use const. If not, use let.
+// Mümkünse, const kullanın. Değilse, let kullanın.
 
-// The customer wants to order some stuff. Here are the details:
+// Müşteri bazı ürünler sipariş etmek istiyor. İşte detaylar:
 const basePrice = 520;
 const discount = 120;
-const shippingCost = 15; // Changed from let to const
-let shippingTime = "5-12 days";
+const shippingCost = 15; // let'ten const'a değiştirildi
+let shippingTime = "5-12 gün";
 
-// Whops! Turns out the shipping will be a bit more complex
-// shippingCost = 15; // No longer needed
-shippingTime = "7-14 days"; // shippingTime remains let as it changes
+// Hopala! Görünüşe göre nakliye biraz daha karmaşık olacak
+// shippingCost = 15; // Artık gerekli değil
+shippingTime = "7-14 gün"; // shippingTime değişebilir durumda kaldı
 
-// Calculating the full price
-const fullPrice = basePrice - discount + shippingCost; // Changed from let to const
+// Tam fiyatı hesaplama
+const fullPrice = basePrice - discount + shippingCost; // let'ten const'a değiştirildi
 
-// Finally, notifying the customer
-console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime);
+// Son olarak, müşteriye bildirme
+console.log("Toplam maliyet: " + fullPrice + ". Teslimat " + shippingTime + " içinde gerçekleşecek.");
 
 
 
