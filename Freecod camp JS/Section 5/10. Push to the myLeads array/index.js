ab0ee7c677de115fc58185bee4012be4f1040fb7
mkkukul <mkkukul@gmail.com>
2024-02-26 21:24:26 +0300

    9

--- Freecod camp JS/Section 5/10. Push to the myLeads array/index.js
+++ Freecod camp JS/Section 5/10. Push to the myLeads array/index.js
@@ -1,3 +1,14 @@
+let myLeads = []; // Boş bir dizi oluşturuldu
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+// Giriş düğmesine tıklandığında "www.awesomelead.com" değerini myLeads dizisine ekleyin
+inputBtn.addEventListener("click", function() {
+    // "www.awesomelead.com" değerini myLeads dizisine ekle
+    myLeads.push("www.awesomelead.com");
+    // Güncellenmiş diziyi konsola yazdır
+    console.log(myLeads);
+});
 
 
 
