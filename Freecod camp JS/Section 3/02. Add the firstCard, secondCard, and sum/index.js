da6afe17dd4f8bd8be198c87b9e01c65864aec32
mkkukul <mkkukul@gmail.com>
2024-02-27 21:22:51 +0300

    2

--- Freecod camp JS/Section 3/02. Add the firstCard, secondCard, and sum/index.js
+++ Freecod camp JS/Section 3/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,3 +1,13 @@
+// 1. İki değişken oluşturun: firstCard ve secondCard. 
+// Bu değişkenlerin değerlerini 2 ile 11 arasında rastgele sayılara ayarlayın.
+let firstCard = 2; // İlk kartın değeri
+let secondCard = 11; // İkinci kartın değeri
+
+// 2. Bir değişken olan sum'u oluşturun ve iki kartın toplamına ayarlayın.
+let sum = firstCard + secondCard; // İki kartın toplam değeri
+
+// 3. Son olarak, sum değişkenini konsola yazdırın.
+console.log(sum); // Toplam kart değerini konsola yazdırın
 
 
 
