f3ba8d18f5ebb958ca74799f10651bb68406002c
mkkukul <mkkukul@gmail.com>
2024-02-28 14:26:43 +0300

    19

--- Freecod camp JS/Section 6/05. Generate sentence/index.js
+++ Freecod camp JS/Section 6/05. Generate sentence/index.js
@@ -1,3 +1,22 @@
+function generateSentence(desc, arr) {
+    // Base string'i oluştur. Açıklama ve dizi uzunluğunu içerir.
+    let baseString = `The ${arr.length} ${desc} are`;
+    
+    // Dizideki her öğe için döngü oluştur
+    for (let i = 0; i < arr.length; i++) {
+        // Base string'e her öğeyi ekleyin ve virgülle ayırın
+        baseString += arr[i] + ",";
+    }
+    
+    // Son olarak oluşturulan cümleyi döndürün
+    return baseString;
+}
+
+// Fonksiyonu kullanarak bir cümle oluşturun ve bu cümleyi bir değişkende saklayın
+const sentence = generateSentence("highest mountains", ["Mount Everest", "K2"]);
+
+// Oluşturulan cümleyi konsola yazdırın
+console.log(sentence);
 
 
 
