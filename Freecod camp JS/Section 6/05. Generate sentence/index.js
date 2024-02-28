00df2f62fd81118d0d0dd14ecb597e6fb17881a3
mkkukul <mkkukul@gmail.com>
2024-02-28 14:26:27 +0300

    17

--- Freecod camp JS/Section 6/05. Generate sentence/index.js
+++ Freecod camp JS/Section 6/05. Generate sentence/index.js
@@ -1,9 +1,9 @@
-function generateSentence(desc, arr) {
-    let baseString = `The ${arr.length} ${desc}are`
-    for (let i= 0; i<arr.length; i++) {
-        baseString +=arr[i]+ ","
-    }
-    return baseString
-}
-const sentence = generateSentence("highest mountains", ["Mount Everst", "K2"])
-console.log(sentence);
+// function generateSentence(desc, arr) {
+    // // let baseString = `The ${arr.length} ${desc}are`
+    // for (let i= 0; i<arr.length; i++) {
+        // baseString +=arr[i]+ ","
+    // }
+    // return baseString
+// }
+// // const sentence = generateSentence("highest mountains", ["Mount Everst", "K2"])
+// console.log(sentence);
