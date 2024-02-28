fbf7eced2da81d2e37d276eb0b1c1151541ae4ee
mkkukul <mkkukul@gmail.com>
2024-02-28 14:27:16 +0300

    20

--- Freecod camp JS/Section 6/05. Generate sentence/index.js
+++ Freecod camp JS/Section 6/05. Generate sentence/index.js
@@ -18,7 +18,7 @@ const sentence = generateSentence("highest mountains", ["Mount Everest", "K2"]);
 // Oluşturulan cümleyi konsola yazdırın
 console.log(sentence);
 
-
+//! Bu kod, bir açıklama ve işlev içerir. İşlev, bir açıklama ve bir dizi alır, daha sonra bu dizideki öğeleri bir cümle oluşturmak için kullanır. Oluşturulan cümle, belirli bir açıklama ve öğelerin sayısıyla başlar, ardından her öğe virgülle ayrılmış bir şekilde eklenir. Son olarak, oluşturulan cümle bir değişkende saklanır ve konsola yazdırılır.
 
 // function generateSentence(desc, arr) {
     // // let baseString = `The ${arr.length} ${desc}are`
